badd96674c1e3895eb3a3e62662d09e5df69206e
이해민 <haemin0623@gmail.com>
2022-10-24 14:45:32 +0900

    프로젝트 생성

---
+++ nike/src/main/java/com/ch/nike/NikeApplication.java
@@ -0,0 +1,13 @@
+package com.ch.nike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikeApplication.class, args);
+	}
+
+}
---
+++ nike/src/test/java/com/ch/nike/NikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.nike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
