b1310db987b5b44bc75367e9ba2752d096587c17
ChangRyul <ok_chang@naver.com>
2022-10-24 20:48:15 +0900

    productdao로 잘못 만든 파일명 productmapper로 수정

--- nike/src/main/java/com/ch/nike/mapper/ProductMapper.java
+++ nike/src/main/java/com/ch/nike/mapper/ProductMapper.java
@@ -3,6 +3,6 @@ package com.ch.nike.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface ProductDao {
+public interface ProductMapper {
 
 }
--- nike/src/main/java/com/ch/nike/service/ProductServiceImpl.java
+++ nike/src/main/java/com/ch/nike/service/ProductServiceImpl.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ch.nike.mapper.ProductDao;
+import com.ch.nike.mapper.ProductMapper;
 
 @Service
 public class ProductServiceImpl {
 	@Autowired
-	private ProductDao pd;
+	private ProductMapper pd;
 	
 	
 }
