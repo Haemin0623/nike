547441a4835621d8e41a56b55161eb43ca17dc0b
이해민 <haemin0623@gmail.com>
2022-10-25 19:24:02 +0900

    SQL 작업중

---
+++ nike/src/exData.sql
@@ -0,0 +1,14 @@
+-- 회원
+insert into member values ('hae@min.com', '이해민', '123', 'M', sysdate, 01012345678, sysdate, 'N');
+insert into member values ('soo@in.com', '박수인', '123', 'F', sysdate, 01012345678, sysdate, 'N');
+insert into member values ('chang@ryul.com', '김창률', '123', 'M', sysdate, 01012345678, sysdate, 'N');
+insert into member values ('sun@ny.com', '최선희', '123', 'F', sysdate, 01012345678, sysdate, 'N');
+
+insert into address values (1, 'hae@min.com', '집', 0311234567, '경기도 용인시 수지구 현암로54', '경기도 용인시 죽전동', 54637, '0동 0호');
+insert into address values (2, 'hae@min.com', '회사', 0311234321, '경기도 땡땡시 땡땡구 땡땡로12', '경기도 땡땡시 땡떙동', 12345, '1호');
+insert into address values (3, 'hae@min.com', '집2', 021234567, '서울특별시 땡땡시 땡땡로54', '서울특별시 땡땡시 땡땡동', , '10동 20호');
+insert into address values (4, 'soo@in.com', '우리집', 021234567, '댕댕시 댕댕로12', '댕댕시 댕댕동', 67637, '0동 0호');
+insert into address values (5, 'soo@in.com', '너네집', 0312341234, '끼얏도 호우시 하이구 바이로14', '끼얏도 호우시 끼호동', 34437, '123동 12호');
+insert into address values (6, 'chang@ryul.com', '회사', 0311432132, '중앙도 정보시 처리구 학원로56번길 24', '중앙도 정보시 학원동', 85337, '5호');
+insert into address values (7, 'chang@ryul.com', '회안사', 0312344567, '너무도 많아시 못하구 겠어로54', '너무도 많아시 못함동', 15217, '12동 101호');
+insert into address values (8, 'sun@ny.com', '기본', 0311236543, '해민도 수인시 창률구 선희로88', '해민도 수인시 창선동', 21337, '10호');
