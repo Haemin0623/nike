369a4b1ac303945ba096de54fd2c63715257836c
이해민 <haemin0623@gmail.com>
2022-11-01 19:35:43 +0900

    DB Configuration 에서 properties 경로 제거

--- nike/src/main/java/com/ch/nike/configuration/DataBaseConfiguration.java
+++ nike/src/main/java/com/ch/nike/configuration/DataBaseConfiguration.java
@@ -10,13 +10,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@PropertySource("classpath:/application.properties")
 public class DataBaseConfiguration {
 
 	@Autowired
